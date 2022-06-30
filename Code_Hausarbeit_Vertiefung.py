aa495212826dbbf9338767bfb347e07e08adee55
s2lascmi <84018628+s2lascmi@users.noreply.github.com>
2022-06-30 10:46:49 +0200

    Code kommentier

--- Code_Hausarbeit_Vertiefung.py
+++ Code_Hausarbeit_Vertiefung.py
@@ -12,20 +12,26 @@ def read_file():
 
 def extract_info(data):
 
-    # Je Dictionary in JSON-Datei einen Dateneintrag für das (Erstellungs-) Jahr und ID hinzufügen
+    # JSON-Datei mit allen Objekten der Sammlung Online einlesen
+    # Je Dictionary (also je Objekt) in JSON-Datei einen Dateneintrag für (Erstellungs-) Jahr und ID hinzufügen
     i = 0
     for data[i] in data:
-        # Jahr als Dictionary-Eintrag hinzufügen (aus Name)
+
+        # Jahr als Dictionary-Eintrag hinzufügen (aus Name des Objekts extrahieren über letzte vier Zeichen)
         year = data[i]["objekt_name"]
         year = year[-4:]
-        # Prüfen, ob year auch wirklich Jahreszahl ist oder nicht, wenn nicht "0" einfügen
+
+        # Prüfen, ob year auch wirklich Jahreszahl ist oder nicht, wenn nicht "102983" einfügen
         if year.isdigit():
             data[i]["objektjahr"] = year
         else:
             data[i]["objektjahr"] = 102983
-        # ID extrahieren
+
+        # ID extrahieren und für Enddatei als "Name" speichern, um Struktur des vorhandenen D3JS-Skripts zu imitieren
+        # (in Enddatei wird das Sammlungsobjekt über die ID referenziert)
         id = data[i]["objekt_id"]
         data[i]["name"] = str(id)
+
         # nicht nötige Key-Value-Paare entfernen
         data[i].pop("objekt_id")
         data[i].pop("objekt_name")
@@ -37,12 +43,14 @@ def extract_info(data):
         data[i].pop("image")
         data[i].pop("total")
         i += 1
+
+    # bereinigte Daten zurückgeben, bestehend aus Objektjahr und ID
     return data
 
 
 def create_1800(data):
 
-    # Leere Listen erstellen
+    # Leere Listen für Jahrzehnte erstellen, in die die Objekte nach Jahr einsortiert werden
     liste_dict_1800_1809 = []
     liste_dict_1810_1819 = []
     liste_dict_1820_1829 = []
@@ -55,7 +63,8 @@ def create_1800(data):
     liste_dict_1890_1899 = []
 
     # jeden Dateneintrag zu Liste mit zugehörigem Entstehungsdatum hinzufügen
-    for entry in data:
+    # Objekt wird hier schon als Dictionary eingefügt mit Key "name" und Value 1, um Struktur des D3JS-Skripts zu imitieren
+    for entry in data: 
         if 1800 <= int(entry["objektjahr"]) <= 1809:
             liste_dict_1800_1809.append({"name": entry["name"], "value": 1})
         elif 1810 <= int(entry["objektjahr"]) <= 1819:
@@ -80,7 +89,8 @@ def create_1800(data):
             print("")
 
 
-    #Dictionaries dritter Ebene erstellen aus vorher generierten Listen der Objekte
+    # Dictionaries dritter Ebene erstellen aus vorher generierten Listen der Objekte
+    # für Enddarstellung in Kuchendiagramm: zweite, detailliertere Zoomstufe
     dict_1800_1809 = {
      "name": "1800-1809",
      "children": liste_dict_1800_1809}
@@ -113,7 +123,8 @@ def create_1800(data):
         "children": liste_dict_1890_1899}
 
 
-    #Dictionaries zweiter Ebene erstellen, indem die vorherigen Dictionaries zusammengefasst werden
+    # Dictionaries zweiter Ebene erstellen, indem die vorherigen Dictionaries zusammengefasst werden
+    # für Enddarstellung in Kuchendiagramm: erste Zoomstufe
     dict_1800_1849 = {
         "name": "1800-1849",
         "children": [dict_1800_1809, dict_1810_1819, dict_1820_1829, dict_1830_1839, dict_1840_1849]}
@@ -122,7 +133,8 @@ def create_1800(data):
         "children": [dict_1850_1859, dict_1860_1869, dict_1870_1879, dict_1880_1889, dict_1890_1899]}
 
 
-    #oberste Ebene erstellen, indem die vorherigen Dictionaries zusammengefasst werde
+    # oberste Ebene erstellen, indem die vorherigen Dictionaries zusammengefasst werde
+    # für Enddarstellung in Kuchendiagramm: Anfangsansicht
     dict_total = {
             "name": "flare",
             "children": [dict_1800_1849, dict_1850_1899]
@@ -132,7 +144,7 @@ def create_1800(data):
 
 def write_file(data):
 
-    # JSON-Datei schreiben lassen, die dann in D3JS verwendet wird
+    # JSON-Datei schreiben, die dann in D3JS-Skript verwendet wird
     file_name = "my-data.json"
     file = open(file_name, "w")
     json.dump(data, file)
